90e565a70ac3ccb8593c08770c480dd8f0c164c6
Randy Lirano <randylirano@randys-mbp.home>
2021-08-01 21:23:28 -0400

    Add query 1 example

--- applicationManager/QueryExamples/Query1.js
+++ applicationManager/QueryExamples/Query1.js
@@ -1,4 +1,4 @@
-// Query1: 
+// Query1: Update "" position by randomly assigning between ["intern", "Software Engineer I", "Software Engineer II", "Software Engineer III"]
 
 const { MongoClient } = require("mongodb");
 
@@ -6,33 +6,70 @@ const uri = "mongodb://localhost:27017";
 
 const client = new MongoClient(uri);
 
-async function run() {
+// retrieve posting collection as an array of posting
+async function getPosting() {
+  const client = new MongoClient(uri);
+
+  try {
+    await client.connect();
+
+    const database = client.db("ApplicationManager");
+    const postingCollection = database.collection("Posting");
+
+    const query = {};
+
+    return await postingCollection.find(query).toArray();
+  } catch(e) {
+    console.log("Error", e);
+  } finally {
+    client.close();
+  }
+}
+
+// fix "" position
+async function fixNullPosition(postingId, postingDoc) {
+  const client = new MongoClient(uri);
   try {
     await client.connect();
 
     // define database to be used
     const database = client.db("ApplicationManager");
     // define collection from database to be used
-    const tweetCollection = database.collection("Posting");
+    const postingCollection = database.collection("Posting");
 
-    // query tweets that are not retweeted
-    const query = { 
-    //   "retweet_count": {$lte: 0}
+    const acceptablePositions = ["Intern", "Software Engineer I", "Software Engineer II", "Software Engineer III"];
+
+    const randIndex = Math.floor(Math.random() * acceptablePositions.length);
+
+    const assignPosition = acceptablePositions[randIndex];
+
+    const filter = {"posting_id": postingId};
+
+    const updateDoc = {
+      $set: {"position": assignPosition}
     };
 
-    // get an array of the query result
-    const queryResults = await tweetCollection.find(query).count();
+    const options = {
+      upsert: true
+    };
 
-    // if ((queryResults.length) === 0) {
-    //   console.log("No documents found!");
-    // } else {
-    //   console.log(queryResults.length);
-    // }
-
-    console.log(queryResults);
-
+    const result = await postingCollection.updateOne(filter, updateDoc, options);
+  } catch (e) {
+    console.log("Error", e);
   } finally {
-    await client.close();
+      //When we are done, close the connection
+      client.close();
   }
 }
-run().catch(console.dir);
+
+async function driver() {
+  const postingArray = await getPosting();
+
+  for (let elem of postingArray) {
+    if (elem.position === "") {
+      await fixNullPosition(elem.posting_id, elem);
+    }
+  }
+}
+
+driver()
